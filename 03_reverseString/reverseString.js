99978bbb37a17e23549593de5b74bda11468e5cf
Raynger Daynger <81805282+rayswerld@users.noreply.github.com>
2022-12-07 20:34:30 -0800

    solution for reverse stringies

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -4,3 +4,19 @@ const reverseString = function() {
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+/*solution
+
+function reverseString(string) {
+    let arr = string.split('');
+    let newArr = arr.reverse();
+    return newArr.join('');
+}
+
+// Do not edit below this line
+module.exports = reverseString;
+
+*/
+
+
